74b7b884806e8b9b6226c369d09ccb4f9f1c69f2
Albert <albert.sales@xarxatecactiva.com>
2022-11-17 01:06:16 +0100

    changes in index.ts and config,ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql from "mysql2";
 
 const PORT=3000;
@@ -6,13 +5,8 @@ const PORT=3000;
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Pau09Joan19Leo20@",
+  password: "password",
   database: "bd_activa_app",
 });
 
 export {PORT, db};  
-=======
-const PORT = 3000;
-
-export { PORT };
->>>>>>> 9666b455645e1444280effc8cf394c1c929a42bc
